b9c82b9d94d57d4b59c08e4c7a311357c132598a
Ramon <ramonjd@gmail.com>
2025-12-30 18:57:39 +1100

    Enhance footnotes block functionality by adding support for attributes and migrating footnotes from post meta. This update includes logic to manage footnotes in both block attributes and post meta for backward compatibility, ensuring a seamless transition for users. Additionally, the footnotes are now initialized correctly when creating new footnotes blocks.

--- packages/block-library/src/footnotes/format.js
+++ packages/block-library/src/footnotes/format.js
@@ -171,7 +171,9 @@ export const format = {
 						rootClientId = getBlockRootClientId( rootClientId );
 					}
 
-					fnBlock = createBlock( 'core/footnotes' );
+					fnBlock = createBlock( 'core/footnotes', {
+						footnotes: [],
+					} );
 
 					insertBlock( fnBlock, undefined, rootClientId );
 				}
