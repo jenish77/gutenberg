5e9b036f1340a7bf2c12d62fa5c4682615a6ac06
Ramon <ramonjd@gmail.com>
2025-12-31 11:52:18 +1100

    Implement footnotes migration logic and improve rendering security. Introduced a custom hook for managing footnotes from block attributes and post meta, ensuring backward compatibility. Enhanced rendering by escaping attributes and content for improved security.

--- packages/block-library/src/footnotes/index.php
+++ packages/block-library/src/footnotes/index.php
@@ -52,9 +52,9 @@ function render_block_core_footnotes( $attributes, $content, $block ) {
 		$aria_label     = sprintf( __( 'Jump to footnote reference %1$d' ), $footnote_index );
 		$block_content .= sprintf(
 			'<li id="%1$s">%2$s <a href="#%1$s-link" aria-label="%3$s">↩︎</a></li>',
-			$footnote['id'],
-			$footnote['content'],
-			$aria_label
+			esc_attr( $footnote['id'] ),
+			wp_kses_post( $footnote['content'] ),
+			esc_attr( $aria_label )
 		);
 		++$footnote_index;
 	}
